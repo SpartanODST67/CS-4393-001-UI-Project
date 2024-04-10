fea02611dafdb778b5b20dcd89f012bbb766ebf0
Hans Landgrebe <hlxbl20@gmail.com>
2024-04-10 13:52:53 -0500

    Input Buttons can now be changed.

---
+++ UI Project/Assets/Scripts/Player Control/Controls.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ControlKeys { UP, LEFT, DOWN, RIGHT, SPRINT, INTERACT };
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Controls")]
+public class Controls : ScriptableObject
+{
+    public List<KeyCode> keys = new List<KeyCode>();
+}
--- UI Project/Assets/Scripts/Player Control/Player Movement/InputHandler.cs
+++ UI Project/Assets/Scripts/Player Control/Player Movement/InputHandler.cs
@@ -7,19 +7,54 @@ public class InputHandler : MonoBehaviour
 {
     [SerializeField] PlayerController player;
     [SerializeField] CinemachineBrain cameraBrain;
+    [SerializeField] Controls controls;
     private Vector3 inputVector = new Vector3();
+    private float horizontalInput;
+    private float verticalInput;
+    private float sprintInput;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(controls.keys[(int) ControlKeys.LEFT]))
+        {
+            horizontalInput = -1;
+        }
+        else if (Input.GetKey(controls.keys[(int) ControlKeys.RIGHT]))
+        {
+            horizontalInput = 1;
+        }
+
+        if (Input.GetKey(controls.keys[(int)ControlKeys.UP]))
+        {
+            verticalInput = 1;
+        }
+        else if(Input.GetKey(controls.keys[(int)ControlKeys.DOWN]))
+        {
+            verticalInput = -1;
+        }
+
+        if (Input.GetKey(controls.keys[(int)ControlKeys.SPRINT]))
+        {
+            sprintInput = 1;
+        }
         inputVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Input.GetAxis("Sprint"));
+        inputVector = new Vector3(horizontalInput, verticalInput, sprintInput);
         player.Sprint(inputVector.z);
         player.MovePlayer(inputVector);
+        horizontalInput = 0;
+        verticalInput = 0;
+        sprintInput = 0;
     }
 
     public float GetInteract()
     {
-        return Input.GetAxis("Interact");
+        float interactInput = 0;
+        if (Input.GetKey(controls.keys[(int) ControlKeys.INTERACT]))
+        {
+            interactInput = 1;
+        }
+        return interactInput;
     }
 
     private void OnDisable()
