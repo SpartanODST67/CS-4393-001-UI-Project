a2fbac5747ac06820d5eab9cdea94a819d68f527
Hans Landgrebe <hlxbl20@gmail.com>
2024-04-14 15:22:33 -0500

    Discard Button reopens inventory in proper mode

--- UI Project/Assets/Scripts/Menu/Inventory/InventoryScreenManager.cs
+++ UI Project/Assets/Scripts/Menu/Inventory/InventoryScreenManager.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryScreenManager : MonoBehaviour
 {
-    [Header("Denotes which back button is activated.")]
+    [Header("Mode 0 - Default Mode. Mode 1 - Gift Menu. Mode 2 - Opened Via Keypress.")]
     [SerializeField] int mode = 0;
     [Header("Closes the menu but does not return control.")]
     [SerializeField] GameObject backButton;
@@ -12,6 +13,11 @@ public class InventoryScreenManager : MonoBehaviour
     [SerializeField] GameObject fullBackButton;
     [Header("For use when the player changes their mind of gifting an object.")]
     [SerializeField] GameObject nevermindButton;
+    [Header("Buttons in the details page")]
+    [SerializeField] GameObject giftButton;
+    [SerializeField] GameObject buyButton;
+    [SerializeField] GameObject sellButton;
+    [SerializeField] GameObject discardButton;
 
     private void OnEnable()
     {
@@ -21,16 +27,36 @@ public class InventoryScreenManager : MonoBehaviour
                 backButton.SetActive(false);
                 fullBackButton.SetActive(false);
                 nevermindButton.SetActive(true);
+                giftButton.SetActive(true);
+                buyButton.SetActive(false);
+                sellButton.SetActive(false);
+                discardButton.SetActive(false);
                 break;
             case 2:
                 backButton.SetActive(false);
                 fullBackButton.SetActive(true);
                 nevermindButton.SetActive(false);
+                giftButton.SetActive(false);
+                buyButton.SetActive(false);
+                sellButton.SetActive(false);
+                discardButton.SetActive(true);
+                discardButton.GetComponent<Button>().onClick.AddListener(() =>
+                {
+                    Open(2);
+                });
                 break;
             default:
                 backButton.SetActive(true);
                 fullBackButton.SetActive(false);
                 nevermindButton.SetActive(false);
+                giftButton.SetActive(false);
+                buyButton.SetActive(false);
+                sellButton.SetActive(false);
+                discardButton.SetActive(true);
+                discardButton.GetComponent<Button>().onClick.AddListener(() =>
+                {
+                    Open(0);
+                });
                 break;
         }
     }
