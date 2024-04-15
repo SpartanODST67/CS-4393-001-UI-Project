2d98819d2c1314833ea975705c7e3b5cb1d836b6
atbriansedano <atbriansedano@gmail.com>
2024-04-14 19:18:47 -0500

    Started pause menu

--- UI Project/Assets/Scripts/Menu/MainMenu.cs
+++ UI Project/Assets/Scripts/Menu/MainMenu.cs
@@ -9,6 +9,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
 
+    public void ToMain(){
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+    }
+
     public void QuitGame(){
         Application.Quit();
     }
